cdbccbbc8754fbec22ccdc505b0e10a31b393938
melisagurdal <80177011+melisagurdal@users.noreply.github.com>
2021-04-30 11:26:25 +0300

    Auth eklendi. Login ve signup ekranı için.

--- src/Modules/Navigation/MainNavigation.js
+++ src/Modules/Navigation/MainNavigation.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import DummyNavigation from './DummyNavigation';
+import AuthNavigation from './AuthNavigation';
 
 const MainNavigation = (props) => {
     return (
         <NavigationContainer>
             <DummyNavigation />
+            :
+            <AuthNavigation />
         </NavigationContainer>
     );
 };
