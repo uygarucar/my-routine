11b6a5793b9fe091ad4b1ed4f06d8e001e25e46d
Uchi91 <uygarucar2@gmail.com>
2021-05-02 13:21:04 +0300

    Lokalizasyondaki "Texts" objesine değerler eklendi

--- src/Modules/Localization/Config/TextNames.js
+++ src/Modules/Localization/Config/TextNames.js
@@ -1,3 +1,32 @@
 export const Texts = {
+    //Sample
     hello: 'hello',
+    //Authorization Screen
+    email: 'email',
+    password: 'password',
+    login: 'login',
+    signUp: 'signUp',
+    username: 'username',
+    //Home Screen
+    home: 'home',
+    //Adding New Screen
+    addNew: 'addNew',
+    routineName: 'routineName',
+    startDate: 'startDate',
+    endDate: 'endDate',
+    routines: 'routines',
+    add: 'add',
+    today: 'today',
+    //Edit Screen
+    edit: 'edit',
+    save: 'save',
+    //Settings Screen
+    settings: 'settings',
+    colorTheme: 'colorTheme',
+    dark: 'dark',
+    light: 'light',
+    language: 'language',
+    english: 'english',
+    turkish: 'turkish',
+    signOut: 'signOut'
 };
--- src/Modules/Localization/LocalizationConstants.js
+++ src/Modules/Localization/LocalizationConstants.js
@@ -2,3 +2,7 @@ export const Locales = {
     english: 'en',
     turkish: 'tr',
 };
+export const LocaleDateFormats = {
+    [Locales.english]: 'MM/DD/YYYY',
+    [Locales.turkish]: 'DD.MM.YYYY'
+}
