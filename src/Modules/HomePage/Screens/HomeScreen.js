bfa4cd39ad75d58aaaf9d08f263b9071593d3481
melisagurdal <80177011+melisagurdal@users.noreply.github.com>
2021-05-03 17:14:03 +0300

    underline transparent deneme

--- src/Modules/HomePage/Screens/HomeScreen.js
+++ src/Modules/HomePage/Screens/HomeScreen.js
@@ -2,23 +2,23 @@ import React from 'react';
 import { SafeAreaView, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { useDispatchChangeTheme, useTheme, useThemedColors, useThemeOptions } from '../../Theming';
 import { useLocalization, tn, useLocaleOptions, useLocale, useDispatchChangeLocale } from '../../Localization';
-import  {Svgs} from '../../../StylingConstants';
+import { Svgs } from '../../../StylingConstants';
 import getStyles from '../styles/HomeScreenStyles';
 import HomePage from '../Components/HomePage';
 
 
 const HomeScreen = props => {
 
-    const currentTheme=useTheme();
+    const currentTheme = useTheme();
     const themedColors = useThemedColors();
     const styles = getStyles(themedColors);
-    const themeOptions=useThemeOptions();
+    const themeOptions = useThemeOptions();
     const changeTheme = useDispatchChangeTheme();
 
-    const currentLocale=useLocale();
-    const loc=useLocalization();
-    const localeOptions=useLocaleOptions();
-    const changeLocale=useDispatchChangeLocale();
+    const currentLocale = useLocale();
+    const loc = useLocalization();
+    const localeOptions = useLocaleOptions();
+    const changeLocale = useDispatchChangeLocale();
 
 
     const _onSelect_Theme = (key) => {
@@ -29,21 +29,24 @@ const HomeScreen = props => {
         changeLocale(key);
     }
 
-    
+
 
     return (
         <View style={styles.container}>
             <SafeAreaView style={{ flex: 1 }}>
-                <TextInput style={styles.routineText}></TextInput>
-                <TextInput style={styles.routineText}></TextInput>
+                <TextInput style={styles.inputText}
+                    underlineColorAndroid='#FFF'
+                    InputProps={{ disableUnderline: true }}
+                ></TextInput>
+                <TextInput style={styles.inputText}></TextInput>
                 <View style={styles.menusContainer}>
                 </View>
             </SafeAreaView>
         </View>
 
-        
+
     );
-    
+
 };
 
 
--- src/Modules/HomePage/styles/HomeScreenStyles.js
+++ src/Modules/HomePage/styles/HomeScreenStyles.js
@@ -1,25 +1,26 @@
 import { StyleSheet } from 'react-native';
 import { cn } from '../../Theming';
-import {  Metrics,Fonts } from '../../../StylingConstants';
+import { Metrics, Fonts } from '../../../StylingConstants';
 
 export default Colors => StyleSheet.create({
-        container: {
-            flex: 1,
-            backgroundColor: Colors[cn.settings.background],
-            padding: Metrics.marginHorizontal,
+    container: {
+        flex: 1,
+        backgroundColor: Colors[cn.settings.background],
+        padding: Metrics.marginHorizontal,
     },
 
-    routineText: {
-        flex:0.08,
+    inputText: {
         fontSize: Fonts.size(16),
         fontFamily: Fonts.type.regular,
         marginHorizontal: 10,
         marginVertical: 10,
         borderRadius: 4,
-        backgroundColor: 'lightgrey',
         alignItems: "center",
         paddingHorizontal: 20,
-        
+        borderColor: 'black',
+        borderWidth: 1,
+
+
     },
-    
+
 });
