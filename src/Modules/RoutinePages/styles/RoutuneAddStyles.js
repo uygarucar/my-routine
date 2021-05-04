a38c90eb42da8e7b99138fc1c5a6bb1008bec8eb
fatmanurafyoncu <fatmanurafyoncu@gmail.com>
2021-05-04 11:08:49 +0300

    Yeni rutin ekleme sayfası

---
+++ src/Modules/RoutinePages/styles/RoutuneAddStyles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+import { cn } from '../../Theming';
+ import {  Metrics,Fonts } from '../../../StylingConstants';
+
+export default Colors=> StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Colors[cn.settings.background],
+        padding: Metrics.marginHorizontal,
+            
+    },
+    textInput:{
+        fontSize: Fonts.size(16),
+        fontFamily: Fonts.type.regular,
+        borderRadius: Metrics.borderRadiusStandard,
+        paddingHorizontal:Metrics.textMargin,
+        alignItems: "center",
+        borderColor: Colors[cn.home.routinesBorder],
+        borderWidth: Metrics.borderWidth,
+        color:Colors[cn.home.routinesText],
+        marginBottom:Metrics.marginBottomTextinput,
+    }
+});
