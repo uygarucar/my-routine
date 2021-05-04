6027b156c23e264eeefa93971400bf09c523e762
Uchi91 <uygarucar2@gmail.com>
2021-05-04 10:53:23 +0300

    Dummy data eklendi

---
+++ src/Modules/HomePage/Components/DummyData.js
@@ -0,0 +1,32 @@
+const dummyRoutine = [
+    {
+        id:1,
+        routineName: 'sabah',
+        startDate: '2021-04-05',
+        endDate: '2021-05-26',
+        routines:['diş fırçala', 'ilacını al']
+    },
+    {
+        id:2,
+        routineName: 'işe giderken',
+        startDate: '2021-04-05',
+        endDate: '2021-05-26',
+        routines:['anahtarını al'] 
+    },
+    {
+        id:3,
+        routineName: 'iş sonrası',
+        startDate: '2021-04-05',
+        endDate: '2021-05-26',
+        routines: ['egzersiz', 'su iç']
+    },
+    {
+        id:4,
+        routineName: 'yatmadan önce',
+        startDate: '2021-04-05',
+        endDate: '2021-05-26',
+        routines: ['Gününü planla']
+    }
+]
+
+export default dummyRoutine;
