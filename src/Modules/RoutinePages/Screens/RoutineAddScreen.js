51c44c0375b5e0ef0d840158a5d68dc5fe049593
fatmanurafyoncu <fatmanurafyoncu@gmail.com>
2021-05-05 21:47:41 +0300

    Bugünün tarihinin eklenmesi

--- src/Modules/RoutinePages/Screens/RoutineAddScreen.js
+++ src/Modules/RoutinePages/Screens/RoutineAddScreen.js
@@ -1,7 +1,10 @@
 import { useNavigation } from '@react-navigation/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, Text, TextInput, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import moment from 'moment';
+
+
 import { useThemedColors } from '../../Theming';
 import getStyles from '../styles/RoutuneAddStyles';
 
@@ -10,6 +13,7 @@ import { cn, useThemedValues } from '../../Theming';
 
 
 const RoutineAddScreen = props => {
+    const [date, setDate] = useState('');
 
     const loc = useLocalization();
     const themedColors = useThemedColors();
@@ -19,13 +23,20 @@ const RoutineAddScreen = props => {
     const _onPress_add = () => {
 
     };
+    
+
+    useEffect(()=>{
+        if(props.value !== undefined){
+            setDate(props.value)
+        }
+    },[])
 
     return (
         <View style={styles.container}>
             <SafeAreaView style={{ flex: 1 }}>
                 <View style={styles.RoutineContainer}>
-                <View style={styles.ViewInput}>
 
+                <View style={styles.ViewInput}>
                     <TextInput
                         style={styles.textInput}
                         placeholder={loc.t(tn.routineName)}
@@ -33,30 +44,30 @@ const RoutineAddScreen = props => {
                         numberOfLines={1}
                     ></TextInput>
                     </View>
-                    <View style={styles.ViewInput}>
 
+                    <View style={styles.ViewInput}>
                     <TextInput
                         style={styles.textInput}
                         placeholder={loc.t(tn.startDate)}
                         placeholderTextColor={themedColors[cn.auth.inputPlaceholder]}
+                        value={date}
+                        editable={false}
                     >
-
                     </TextInput>
-                    <TouchableOpacity style={styles.today} >
+                    <TouchableOpacity style={styles.today} onPress={()=>setDate(moment().format('YYYY-MM-DD'))}>
                             <Text style={styles.todayText}>{loc.t(tn.today)}</Text>
                         </TouchableOpacity>
-
                     </View>
-                    <View style={styles.ViewInput}>
 
+                    <View style={styles.ViewInput}>
                     <TextInput
                         style={styles.textInput}
                         placeholder={loc.t(tn.endDate)}
                         placeholderTextColor={themedColors[cn.auth.inputPlaceholder]}
                     ></TextInput>
                     </View>
-                    <View style={styles.ViewInput}>
 
+                    <View style={styles.ViewInput}>
                     <TextInput
                         style={[styles.RoutineInput, styles.textInput]}
                         placeholder={loc.t(tn.routines)}
